be20445f102752c746c41ebe95e515e623bc5441
Grangy <m.dolia2017@yandex.ru>
2025-03-10 21:53:11 +0300

    Yes i understand

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 // @ts-check
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    turbopack: false, // отключаем Turbopack, чтобы использовать Webpack
+  },
+  // здесь можно добавить другие опции конфигурации
 }
- 
+
 module.exports = nextConfig
