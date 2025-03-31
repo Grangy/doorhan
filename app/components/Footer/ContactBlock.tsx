b0a49d4c7117035590cfe3618a1c007276b9c29a
Grangy <m.dolia2017@yandex.ru>
2025-03-31 21:02:29 +0300

    deleteoffice

--- app/components/Footer/ContactBlock.tsx
+++ app/components/Footer/ContactBlock.tsx
@@ -3,7 +3,7 @@ export default function ContactBlock() {
       <div className="mb-8">
         <h4 className="text-lg font-semibold mb-4">Контакты</h4>
         <ul className="space-y-2 text-gray-400">
-          <li>ул. Маршала Советского Союза Буденного С.М., 32И, Симферопольэтаж 5, офис 103</li>
+          <li>ул. Маршала Советского Союза Буденного С.М., 32И, Симферопольэтаж 5</li>
           <li>Телефон: +7 (928) 274-64-68</li>
           <li>Email: info@doorhan-crimea.com</li>
           <li>Режим работы: Пн-Пт 9:00-18:00</li>
