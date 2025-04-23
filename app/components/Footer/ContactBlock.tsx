0d5b571a925438f5dbafe78a4d73802b471abaa8
Grangy <m.dolia2017@yandex.ru>
2025-04-23 16:45:41 +0300

    update footer

--- app/components/Footer/ContactBlock.tsx
+++ app/components/Footer/ContactBlock.tsx
@@ -4,8 +4,8 @@ export default function ContactBlock() {
         <h4 className="text-lg font-semibold mb-4">Контакты</h4>
         <ul className="space-y-2 text-gray-400">
           <li>ул. Маршала Советского Союза Буденного С.М., 32И, Симферопольэтаж 5</li>
-          <li>Телефон: +7 978 263‑95‑21</li>
-          <li>Email: info@doorhan-crimea.com</li>
+          <li>Телефон: <a href="tel:+79782639521">+7 978 263‑95‑21</a></li>
+          <li>Email: <a href="mailto:doorhan82@yandex.ru">doorhan82@yandex.ru</a></li>
           <li>Режим работы: Пн-Пт 9:00-18:00</li>
         </ul>
       </div>
