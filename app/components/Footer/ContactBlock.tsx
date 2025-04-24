68a50a75412d085dbf9449bc89e4c9b2fcdb3b73
Grangy <m.dolia2017@yandex.ru>
2025-04-24 20:10:51 +0300

    update-mail

--- app/components/Footer/ContactBlock.tsx
+++ app/components/Footer/ContactBlock.tsx
@@ -5,7 +5,7 @@ export default function ContactBlock() {
         <ul className="space-y-2 text-gray-400">
           <li>ул. Маршала Советского Союза Буденного С.М., 32И, Симферопольэтаж 5</li>
           <li>Телефон: <a href="tel:+79782639521">+7 978 263‑95‑21</a></li>
-          <li>Email: <a href="mailto:doorhan82@yandex.ru">doorhan82@yandex.ru</a></li>
+          <li>Email: <a href="mailto:zakaz@doorhan-zavod.ru">zakaz@doorhan-zavod.ru</a></li>
           <li>Режим работы: Пн-Пт 9:00-18:00</li>
         </ul>
       </div>
